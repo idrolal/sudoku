9741d547b5ce2d8f8bc5207ef0dac76766c8c383
Anna <anna.si4eva3000@gmail.com>
2021-12-03 16:47:16 +0300

    Random number

--- sudoku.js
+++ sudoku.js
@@ -4,42 +4,56 @@
 // your solver has tried to solve it.
 // How you represent your board is up to you!
 
+// const fs = require('fs')
+// const read = fs.readFileSync('/sudoku-puzzles.txt', 'utf-8').split('\n');
+
 let str = '--5-3--819-285--6-6----4-5---74-283-34976---5--83--49-15--87--2-9----6---26-495-3'
-function solve(boardString) {
+const table = prettyBoard(str)
+console.log(a(table))
 
+function a (arr) {
+	for (let j = 0; j < arr.length; j++){
+	for(let i = 0; i < arr.length; i++){
+		
+	return arr[i].map(el => el.replace('-', Math.floor(Math.random(1)*9)))
+	} 
+}
 }
 
+function solved(table) {
 
-// Returns a boolean indicating whether
-// or not the provided board is solved.
-// The input board will be in whatever
-// form `solve` returns.
-function isSolved(board) {
-
+	for(let i = 0; i < table.length; i++){
+		for(let j = 0; j < 9; j++){
+			if(table[i][j] === '-'){
+				return  table[i][j] = Math.floor(Math.random(1)*9)
+				} else {
+				return table[i][j] = table[i][j]
+			}
+		}
+	}
 }
 
-
-// Takes in a board in some form and
-// returns a String that's well formatted
-// for output to the screen.
-// The input board will be in whatever
-// form `solve` returns.
-function prettyBoard(board) {
+function prettyBoard (board) {
 	let res = []
 
 		for(let i = 0; i < board.length; i+=9){
 			res.push(board.slice(i, [i+9]))
 		}
 
-	return res.map(a=>a.split(''))
+	return res.map(a=> a.split(''))
 
 }
-console.log(prettyBoard(str))
+
+function isSolved(board) {
+
+}
+
+
 
 
 // Exports all the functions to use them in another file.
 module.exports = {
-	solve: solve,
+	solved: solved,
 	isSolved: isSolved,
 	prettyBoard: prettyBoard
 }
